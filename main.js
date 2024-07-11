62a43d2b16ae39b7e7234a45149a7a27a18ebe57
John <128350391+alpaOo@users.noreply.github.com>
2024-07-11 19:37:10 +0800

    Integrate 'crawlPage' function into main

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
-function main() {
+import { crawlPage, getURLsFromHTML } from "./crawl.js";
+
+async function main() {
   if (process.argv.length === 3) {
     console.log("Starting...");
     console.log(`Web crawler starting at ${process.argv[2]}...`);
   } else {
     console.log(new Error("Received 0 arguments; 1 argument required."));
   }
+
+  const htmlPage = await crawlPage(process.argv[2]);
+  const urlArray = getURLsFromHTML(htmlPage, process.argv[2]);
+  console.log(urlArray);
 }
 
 main();
