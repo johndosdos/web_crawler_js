dda2daf4546d491cf33c9cdaa064b8a6ea0f8296
John <128350391+alpaOo@users.noreply.github.com>
2024-07-13 02:10:33 +0800

    Refactor 'main' for clarity, prepend HTTP protocol
    * Prepend HTTPS protocol during program start to normalize URL

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
 import { crawlPage, getURLsFromHTML } from "./crawl.js";
 
 async function main() {
+  let urlArg = process.argv[2];
+
+  if (!urlArg.startsWith("https://")) {
+    urlArg = "https://" + urlArg;
+  }
+
   if (process.argv.length === 3) {
     console.log("Starting...");
-    console.log(`Web crawler starting at ${process.argv[2]}...`);
+    console.log(`Web crawler starting at ${urlArg}...`);
   } else {
     console.log(new Error("Received 0 arguments; 1 argument required."));
   }
