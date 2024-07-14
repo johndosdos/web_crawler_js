8b77427866a06f78e5e702d2c54a8b17b5f24b1f
John <128350391+alpaOo@users.noreply.github.com>
2024-07-14 17:22:15 +0800

    Fix indentation

--- crawl.js
+++ crawl.js
@@ -1,63 +1,63 @@
 import { JSDOM } from "jsdom";
 
 function normalizeURL(url) {
-  const newUrl = new URL(url);
-  return newUrl.hostname + newUrl.pathname;
+    const newUrl = new URL(url);
+    return newUrl.hostname + newUrl.pathname;
 }
 
 function getURLsFromHTML(htmlBody, baseURL) {
-  const dom = new JSDOM(htmlBody);
-  const anchorTags = dom.window.document.querySelectorAll("a");
-  const urlArray = [];
-  for (const tag of anchorTags) {
-    const fullURL = new URL(tag.href, baseURL).href;
-    urlArray.push(fullURL);
-  }
-  return urlArray;
+    const dom = new JSDOM(htmlBody);
+    const anchorTags = dom.window.document.querySelectorAll("a");
+    const urlArray = [];
+    for (const tag of anchorTags) {
+        const fullURL = new URL(tag.href, baseURL).href;
+        urlArray.push(fullURL);
+    }
+    return urlArray;
 }
 
 async function crawlPage(baseURL, currentURL = baseURL, pages = {}) {
-  try {
-    const currentURLnorm = new URL(currentURL, baseURL).toString();
+    try {
+        const currentURLnorm = new URL(currentURL, baseURL).toString();
 
-    if (!(currentURLnorm in pages)) {
-      pages[currentURLnorm] = 1;
-    } else {
-      pages[currentURLnorm] += 1;
-      return pages;
+        if (!(currentURLnorm in pages)) {
+            pages[currentURLnorm] = 1;
+        } else {
+            pages[currentURLnorm] += 1;
+            return pages;
+        }
+
+        const response = await fetch(currentURL, {
+            method: "GET",
+            mode: "cors",
+        });
+
+        if (response.status >= 400) {
+            console.log(`Error: ${response.status} at ${currentURLnorm}`);
+            return pages;
+        }
+
+        const contentType = response.headers.get("content-type");
+        if (!contentType.includes("text/html")) {
+            console.log(`Error: Non-HTML content-type at ${currentURLnorm}`);
+            return pages;
+        }
+
+        const HTMLPage = await response.text();
+        const pageURLs = getURLsFromHTML(HTMLPage, currentURLnorm);
+
+        for (const url of pageURLs) {
+            const urlNorm = new URL(url, baseURL);
+
+            if (urlNorm.hostname !== new URL(baseURL).hostname) {
+                continue;
+            }
+            await crawlPage(baseURL, urlNorm, pages);
+        }
+    } catch (error) {
+        console.log(`Error: ${error} at ${currentURL}`);
     }
-
-    const response = await fetch(currentURL, {
-      method: "GET",
-      mode: "cors",
-    });
-
-    if (response.status >= 400) {
-      console.log(`Error: ${response.status} at ${currentURLnorm}`);
-      return pages;
-    }
-
-    const contentType = response.headers.get("content-type");
-    if (!contentType.includes("text/html")) {
-      console.log(`Error: Non-HTML content-type at ${currentURLnorm}`);
-      return pages;
-    }
-
-    const HTMLPage = await response.text();
-    const pageURLs = getURLsFromHTML(HTMLPage, currentURLnorm);
-
-    for (const url of pageURLs) {
-      const urlNorm = new URL(url, baseURL);
-
-      if (urlNorm.hostname !== new URL(baseURL).hostname) {
-        continue;
-      }
-      await crawlPage(baseURL, urlNorm, pages);
-    }
-  } catch (error) {
-    console.log(`Error: ${error} at ${currentURL}`);
-  }
-  return pages;
+    return pages;
 }
 
 export { normalizeURL, getURLsFromHTML, crawlPage };
